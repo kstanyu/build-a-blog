8cd19d82de96a725399252ae560fa05fa078fcf0
kstanyu <kstanyu@gmail.com>
2018-01-30 20:00:28 -0600

    code cleanup

--- signup_info_checks.py
+++ signup_info_checks.py
@@ -23,14 +23,6 @@ def is_a_match(usr_str_a, usr_str_b):
         match = True
     return match
 
-#def char_search(main_str, characters):
- #   chars_in = True
- #   for char in characters:
- #       if char not in main_str:
- #           chars_in = False
- #           break
- #   return chars_in
-
 def is_email(usr_email):
     valid_email = False
     if((usr_email.count("@") == 1) and (usr_email.count(".") == 1) and not contains_aspace(usr_email)) or usr_email == "": 
